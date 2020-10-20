e1e02325f7bf1435380fd7db154f5aa6c2b71809
LH <lh@lhdv.net>
2020-10-20 16:24:31 -0300

    Bootstrap theme Projects views

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :projects
+  
+  resources :tenants do
+    resources :projects
+  end
+  
   resources :members
   get 'home/index'
   root :to => "home#index"
