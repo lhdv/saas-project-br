09cc8b0e1b9704001f4ee8e4c5e501f0fc25acd8
LH <lh@lhdv.net>
2020-10-20 21:15:36 -0300

    Add Artifact model and relate it to Project model

---
+++ app/models/artifact.rb
@@ -0,0 +1,22 @@
+class Artifact < ApplicationRecord
+  MAX_FILESIZE = 10.megabytes
+  
+  attr_acsessor :upload
+
+  belongs_to :project
+
+  validates :name, presence: true
+  validates :name, uniqueness: true
+
+  validates :upload, presence: true
+
+  validate :uploaded_file_size
+
+  private
+
+  def uploaded_file_size
+    if upload
+      errors.add(:upload, "File too large. Must be less than #{self.class::MAX_FILESIZE}") unless upload.size <= self.class::MAX_FILESIZE
+    end
+  end
+end
