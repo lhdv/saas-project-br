8cacc49ab197e6d9beff2c44eb46cfea2972690c
LH <lh@lhdv.net>
2020-10-21 22:22:32 -0300

    Add JavaScript to handle Stripe payments

--- app/assets/javascripts/credit_card_form.js
+++ app/assets/javascripts/credit_card_form.js
@@ -1,12 +1,26 @@
+// function to get params from URL
+function GetURLParameter(sParam) {
+    var sPageURL = windows.location.search.substring(1);
+    var sURLVariables = sPageURL.split('&');
+    for (var i = 0; i < sURLVariables.length; i++) {
+        var sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] == sParam) {
+            return sParameterName[1];
+        }
+    }
+};
+
 $(document).on('ready turbolinks:load', function() {
     
     var show_error, stripeResponseHandler, submitHandler;
     
+    // function to handle the submit of the form and intercept the default event    
     submitHandler = function(event) {
         var $form = $(event.target);
-
+        
         $form.find("input[type=submit]").prop("disabled", true);
-
+        
         // If Stripe was initialized correctly this will create a token using credit card info
         if (Stripe) {
             Stripe.card.createToken($form, stripeResponseHandler);
@@ -15,9 +29,39 @@ $(document).on('ready turbolinks:load', function() {
         }
         return false;
     };
-
+    
+    // Initiate submit handler listener for any form with class cc_form
     $('.cc_form').on('submit', submitHandler);
 
+    // handle event of plan drop down changing
+    var handlePlanChange = function(plan_type, form) {
+        $form = $(form);
+
+        if (plan_type == undefined) {
+            plan_type = $('#tenant_plan :selected').val();
+        }
+
+        if (plan_type === 'premium') {
+            $('[data-stripe]').prop('required', true);
+            $form.off('submit');
+            $form.on('submit', submitHandler);
+            $('[data-stripe]').show();
+        } else {
+            $('[data-stripe]').hide();
+            $form.off('submit');
+            $('[data-stripe]').removeProp('required');
+        }
+    };
+
+    // Setup plan change event listener #tenant_plan id in the forms for class cc_form
+    $('#tenant_plan').on('change', function(e) {
+        handlePlanChange($('#tenant_plan :selected').val(), ".cc_form");
+    });
+
+    // call plan change handler so that the plan is set correctly in the drop down when the page loads
+    handlePlanChange(GetURLParameter('plan'), '.cc_form');
+    
+    // function to handle the token received from Stripe and remove credit cards fields
     stripeResponseHandler = function(status, response) {
         var token, $form;
         
@@ -32,7 +76,7 @@ $(document).on('ready turbolinks:load', function() {
             $form.append($("<input type=\"hidden\" name=\"payment[token]\" />").val(token));
             $("[data-stripe=number]").remove();
             $("[data-stripe=cvc]").remove();
-            $("[data-stripe=exp-year]").remove();
+             $("[data-stripe=exp-year]").remove();
             $("[data-stripe=exp-month]").remove();
             $("[data-stripe=label]").remove();
             
