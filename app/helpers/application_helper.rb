0b09ddd19c572183324ca5073a21dadceb5f82bf
LH <lh@lhdv.net>
2020-10-22 17:22:56 -0300

    Add Tenenats Controller to handle payments

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -35,4 +35,9 @@ module ApplicationHelper
   def s3_link(tenant_id, artifact_key)
     link_to artifact_key, "#{artifact_key}", target: 'new', class: 'main-link'
   end
+
+  def class_name_for_tenant_form(tenant)
+    return "cc_form" if tenant.payment.blank?
+    ""
+  end
 end
